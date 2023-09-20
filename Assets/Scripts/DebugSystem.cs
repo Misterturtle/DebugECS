79a55804bde4c74dfabdfa411bc20f91ee57e903
unknown <ryan@conaway58.com>
2023-09-19 21:19:42 -0400

    Solved by setting InputEvent back to default the frame after it was set

--- Assets/Scripts/DebugSystem.cs
+++ Assets/Scripts/DebugSystem.cs
@@ -5,10 +5,9 @@ using Unity.NetCode;
 using Unity.Physics.Systems;
 using UnityEngine;
 
-[GhostComponent(PrefabType=GhostPrefabType.AllPredicted, OwnerSendType = SendToOwnerType.SendToNonOwner)]
 public struct DebugInput : IInputComponentData
 {
-    [GhostField] public InputEvent DebugEvent;
+    public InputEvent DebugEvent;
 }
 
 public struct DebugTag : IComponentData
@@ -16,44 +15,33 @@ public struct DebugTag : IComponentData
 }
 
 [BurstCompile]
-[WorldSystemFilter(WorldSystemFilterFlags.ClientSimulation | WorldSystemFilterFlags.ThinClientSimulation, WorldSystemFilterFlags.ClientSimulation)]
+[WorldSystemFilter(WorldSystemFilterFlags.ClientSimulation | WorldSystemFilterFlags.ThinClientSimulation)]
 [UpdateInGroup(typeof(GhostInputSystemGroup))]
 public partial struct DebugSystem : ISystem
 {
-    public void OnCreate(ref SystemState state)
-    {
-        Debug.Log("On Create DebugSystem");
-    }
-
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
         var commandBuffer = new EntityCommandBuffer(Allocator.TempJob);
-        
-        state.Dependency.Complete();
+
         new DebugJob
         {
-            ECB = commandBuffer
-        }.Run();
+            ECB = commandBuffer,
+            ShouldSetInputTag = state.GetComponentLookup<DebugTag>(true)
+        }.Schedule();
 
         state.Dependency.Complete();
         
         commandBuffer.Playback(state.EntityManager);
-        
     }
 }
 
 
-[UpdateInGroup(typeof(PhysicsSystemGroup))]
-[UpdateBefore(typeof(PhysicsInitializeGroup))]
+[UpdateInGroup(typeof(PredictedSimulationSystemGroup))]
+[WorldSystemFilter(WorldSystemFilterFlags.ServerSimulation)]
 [BurstCompile]
 public partial struct ServerDebugSystem : ISystem
 {
-    public void OnCreate(ref SystemState state)
-    {
-        Debug.Log("On Create ServerDebugSystem");
-    }
-
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
@@ -74,14 +62,23 @@ public partial struct ServerDebugSystem : ISystem
 [BurstCompile]
 public partial struct DebugJob : IJobEntity
 {
+    [ReadOnly]
+    public ComponentLookup<DebugTag> ShouldSetInputTag;
     public EntityCommandBuffer ECB;
-    
+
     [BurstCompile]
-    private void Execute(Entity entity, DebugTag debugTag, ref DebugInput debugInput)
+    private void Execute(Entity entity, ref DebugInput debugInput)
     {
         Debug.Log($"Running Debug Job");
-        debugInput.DebugEvent.Set();
-        ECB.RemoveComponent<DebugTag>(entity);
+        debugInput.DebugEvent = default;
+
+        bool shouldSetInput = ShouldSetInputTag.HasComponent(entity);
+        if (shouldSetInput)
+        {
+            Debug.Log($"Setting input");
+            debugInput.DebugEvent.Set();
+            ECB.RemoveComponent<DebugTag>(entity);
+        }
     }
 }
 
